ce41a4f9d3a48bea356dfed96d640bcf20537eec
Julian Pasquale <julianagustin78@gmail.com>
2020-04-20 19:13:36 -0300

    Connects with github dataset

--- routes/search.js
+++ routes/search.js
@@ -4,41 +4,24 @@ var router         = express.Router()
 
 require('dotenv').config()
 
-/* GET users listing. */
+const bigquery = new BigQuery()
+
 router.get('/', function(req, res, next) {
-  query().then((result) => res.send(result))
+  call().then((result) => res.send(result))
 })
 
-async function query() {
-  // Queries the U.S. given names dataset for the state of Texas.
+call = async () => (
+  bigquery.createQueryJob('SELECT repo_name FROM `bigquery-public-data.github_repos.sample_repos` LIMIT 10').then((data) => {
+    const job         = data[0]
+    const apiResponse = data[1]
 
-  const bigquery = new BigQuery()
-
-  const query = `SELECT name
-    FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
-    WHERE state = 'TX'
-    LIMIT 100`
-
-  // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-  const options = {
-    query: query,
-    // Location must match that of the dataset(s) referenced in the query.
-    location: 'US',
-  }
-
-  // Run the query as a job
-  const [job] = await bigquery.createQueryJob(options)
-  console.log(`Job ${job.id} started.`)
-
-  // Wait for the query to finish
-  const [rows] = await job.getQueryResults()
-
-  // Print the results
-  console.log('Rows:')
-  rows.forEach(row => console.log(row))
-
-  return rows
-}
-// [END bigquery_query]
+    console.log(`Job ${job.id} started.`)
+    
+    return job.getQueryResults().then((data) => {
+      console.log(data)
+      return data[0];
+    });
+  })
+)
 
 module.exports = router
