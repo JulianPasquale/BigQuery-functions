83508d7bac8de2be363324dce63dba7a4e1cf51a
Julian Pasquale <julianagustin78@gmail.com>
2020-04-23 21:04:26 -0300

    Websocket server configured and working

--- api/v2/router.js
+++ api/v2/router.js
@@ -1,20 +1,30 @@
-const { Router } = require('express')
+let { Router } = require('express')
 
 // Functions
 const repositories = require('./functions/repositories')
 
 // Handlers.
-const success   = require('./handlers/success')
+// const success   = require('./handlers/success')
 const error     = require('./handlers/exception')
 const not_found = require('./handlers/not_found')
 
 // Middlewares.
-const job_config = require('./middlewares/job_config')
+// const job_config = require('./middlewares/job_config')
 
 // Routes.
 const router = Router()
 
-router.get('/repositories', job_config, repositories, success)
+// router.get('/repositories', job_config, repositories, success)
+router.ws('/echo', function(ws, req) {
+  ws.send(['mundo'])
+
+  ws.on('message', (msg) => {
+    // Broadcast msg from 1 to all clients.
+    wss.getWss().clients.forEach((client) => 
+      client.send(msg.data)
+    )
+  })
+})
 
 // Only debug purpose
 router.get('/err', (req, res, next) => next(next(new Error('custom'))))
--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const path         = require('path')
 const cookieParser = require('cookie-parser')
 const logger       = require('morgan')
 
+const app     = express()
+var expressWs = require('express-ws')(app)
+
+// Global routers.
 const indexRouter = require('./routes/index')
 const apiV1Router = require('./api/v1/router')
 const apiV2Router = require('./api/v2/router')
 
-const app = express()
+expressWs.applyTo(apiV2Router)
 
+// Sets up websockets in global router.
+// const expressWs = require('express-ws')(app)
+
+// Middlewares.
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -19,8 +27,27 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 
-// API routes
+// API routes.
 app.use('/api/v1', apiV1Router)
 app.use('/api/v2', apiV2Router)
+
+
+// expressWs.getWss().on('connection', function(ws) {
+//   console.log('connection open');
+// });
+
+// app.get('/api/v2/echo', function(req, res, next){
+//   console.log('get route', req.testing);
+//   res.end();
+// });
+
+// app.ws('/api/v2/echo', function(ws, req) {
+//   ws.on('message', function(msg) {
+//     console.log(msg);
+//     res.send('hola')
+//   });
+//     res.send('hola')
+//     console.log('socket', req.testing);
+// });
 
 module.exports = app
